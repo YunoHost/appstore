92a40b4643d833e85fa0a4464a80dfb2330acbbf
Félix Piédallu <felix@piedallu.me>
2026-03-05 14:37:18 +0100

    utils: Simplify code, use pathlib

--- src/appstore/utils.py
+++ src/appstore/utils.py
@@ -1,16 +1,17 @@
 import base64
 import json
-import os
 import tomllib
 from pathlib import Path
+from typing import Any
 
 import pycmarkgfm
 from emoji import emojize
 from flask import request
 
+TRANSLATIONS_DIR = (Path(__file__).parent / "translations").resolve()
 AVAILABLE_LANGUAGES = [
     "en",
-    *[path.name for path in Path("translations").iterdir() if path.is_dir()],
+    *[path.name for path in TRANSLATIONS_DIR.iterdir() if path.is_dir()],
 ]
 
 DATA_DIR: Path | None = None
@@ -121,51 +122,41 @@ def human_to_binary(size: str) -> int:
 def get_app_md_and_screenshots(app_folder: Path, infos: dict[str, Any]) -> None:
     locale = get_locale()
 
-    if locale != "en" and os.path.exists(
-        os.path.join(app_folder, "doc", f"DESCRIPTION_{locale}.md")
-    ):
-        description_path = os.path.join(app_folder, "doc", f"DESCRIPTION_{locale}.md")
-    elif os.path.exists(os.path.join(app_folder, "doc", "DESCRIPTION.md")):
-        description_path = os.path.join(app_folder, "doc", "DESCRIPTION.md")
-    else:
-        description_path = None
-    if description_path:
-        with open(description_path) as f:
-            infos["full_description_html"] = emojize(
-                pycmarkgfm.gfm_to_html(f.read()), language="alias"
-            )
-    else:
-        infos["full_description_html"] = infos["manifest"]["description"][locale]
+    def get_value(files: list[Path], default: str) -> str:
+        for file in files:
+            if file.exists():
+                return emojize(
+                    pycmarkgfm.gfm_to_html(file.read_text()), language="alias"
+                )
+        return default
 
-    if locale != "en" and os.path.exists(
-        os.path.join(app_folder, "doc", f"PRE_INSTALL_{locale}.md")
-    ):
-        pre_install_path = os.path.join(app_folder, "doc", f"PRE_INSTALL_{locale}.md")
-    elif os.path.exists(os.path.join(app_folder, "doc", "PRE_INSTALL.md")):
-        pre_install_path = os.path.join(app_folder, "doc", "PRE_INSTALL.md")
-    else:
-        pre_install_path = None
-    if pre_install_path:
-        with open(pre_install_path) as f:
-            infos["pre_install_html"] = emojize(
-                pycmarkgfm.gfm_to_html(f.read()), language="alias"
-            )
+    infos["full_description_html"] = get_value(
+        [
+            app_folder / "doc" / f"DESCRIPTION_{locale}.md",
+            app_folder / "doc" / "DESCRIPTION.md",
+        ],
+        infos["manifest"]["description"][locale],
+    )
+
+    pre_install = get_value(
+        [
+            app_folder / "doc" / f"PRE_INSTALL_{locale}.md",
+            app_folder / "doc" / "PRE_INSTALL.md",
+        ],
+        "",
+    )
+    if pre_install:
+        infos["pre_install_html"] = pre_install
 
     infos["screenshot"] = None
-
-    screenshots_folder = os.path.join(app_folder, "doc", "screenshots")
-
-    if os.path.exists(screenshots_folder):
-        with os.scandir(screenshots_folder) as it:
-            for entry in it:
-                ext = os.path.splitext(entry.name)[1].replace(".", "").lower()
-                if entry.is_file() and ext in ("png", "jpg", "jpeg", "webp", "gif"):
-                    with open(entry.path, "rb") as img_file:
-                        data = base64.b64encode(img_file.read()).decode("utf-8")
-                        infos["screenshot"] = (
-                            f"data:image/{ext};charset=utf-8;base64,{data}"
-                        )
-                    break
+    screenshots_folder = app_folder / "doc" / "screenshots"
+    if screenshots_folder.exists():
+        for file in screenshots_folder.iterdir():
+            ext = file.suffix.lower()
+            if file.is_file() and ext in ("png", "jpg", "jpeg", "webp", "gif"):
+                data = base64.b64encode(file.read_bytes()).decode("utf-8")
+                infos["screenshot"] = f"data:image/{ext};charset=utf-8;base64,{data}"
+                break
 
     ram_build_requirement = infos["manifest"]["integration"]["ram"]["build"]
     infos["manifest"]["integration"]["ram"]["build_binary"] = human_to_binary(
